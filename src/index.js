1051662c5909f2bfef5c5bb2959fb28f77bdeba2
Tashila Fernando <tashila97@hotmail.com>
2020-01-12 12:11:21 +0530

    Init Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+import Spinner from './Spinner'
+import * as serviceWorker from './serviceWorker'
+import firebase from './firebase'
+import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	withRouter
+} from 'react-router-dom'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './reducers/index'
+import { setUser, clearUser } from './actions/index'
+
+const store = createStore(rootReducer, composeWithDevTools())
+
+class Root extends React.Component {
+	componentDidMount() {
+		firebase.auth().onAuthStateChanged(user => {
+			if (user) {
+				this.props.setUser(user)
+				this.props.history.push('/')
+			} else {
+				this.props.history.push('/login')
+				this.props.clearUser()
+			}
+		})
+	}
+
+	render() {
+		return this.props.isLoading ? (
+			<Spinner />
+		) : (
+			<Switch>
+				<Route exact path='/' component={App} />
+				<Route path='/login' component={Login} />
+				<Route path='/register' component={Register} />
+			</Switch>
+		)
+	}
+}
+
+const mapStateFromProps = state => ({
+	isLoading: state.user.isLoading
+})
+
+const RootWithAuth = withRouter(
+	connect(mapStateFromProps, { setUser, clearUser })(Root)
+)
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Router>
+			<RootWithAuth />
+		</Router>
+	</Provider>,
+	document.getElementById('root')
+)
+
+serviceWorker.unregister()
